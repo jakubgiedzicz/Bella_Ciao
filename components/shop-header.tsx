7b59fdbdfa1774da2c9a8016da93cb5a8f60b989
JakubGiedzicz <97766457+KubutekG@users.noreply.github.com>
2023-07-05 13:30:40 +0200

    Add files via upload

--- components/shop-header.tsx
+++ components/shop-header.tsx
@@ -1,21 +1,17 @@
 "use client";
 import Image from "next/image";
 import Logo from "@/public/Logo.webp";
-import Cart from "@/public/shopping-cart-outline.svg";
 import styles from "@/styles/shop-header.module.css";
 import { useEffect, useRef, useState } from "react";
 import Link from "next/link";
 export default function ShopHeader() {
-  /* Navbar visibility; changes on user scroll */
-  const [visible, setVisible] = useState(true);
-
   /* Navbar list items after clicking on designed button */
   const [toggled, setToggled] = useState(false);
 
   /* Prevent useEffect to display navbar on mount */
   const effectRan = useRef(false);
 
-  /* const handleBurgerClick = () => {
+  const handleBurgerClick = () => {
     if (toggled === true) {
       setToggled(false);
     } else {
@@ -23,78 +19,39 @@ export default function ShopHeader() {
     }
   };
 
-  const handleScroll = (event: any) => {
-    if (event.deltaY < 0) {
-      setVisible(true);
-    } else {
-      setVisible(false);
-    }
-  };
-
-  const hideOnScroll = () => {
-    const header: any = document.getElementById("header");
-    const navbar: any = document.getElementsByClassName(styles.navbar);
-    if (header.style.display === "block" && navbar[0].style.width === "auto") {
-      setToggled(false);
-      setVisible(false);
-    }
-  };
-
-  const toggleNavbar = () => {
-    toggleHeader();
-    let button: any = document.getElementsByClassName(styles.nav_toggle);
-    let links: any = document.getElementsByClassName(styles.nav_links);
-    let links_wrap: any = document.getElementsByClassName(
-      styles.list_toggle_wrap
-    );
-    button[0].classList.toggle(styles.open);
-    links[0].classList.toggle(styles.open);
-    links_wrap[0].classList.toggle(styles.open);
-  };
   const toggleHeader = () => {
-    if (document.documentElement.clientWidth < 760) {
-      const header: any = document.getElementById("header");
-      const navbar: any = document.getElementsByClassName(styles.navbar);
-      if (visible === true) {
-        header.style.display = "block";
-      } else {
-        header.style.display = "none";
+    const body = document.body
+    const modal = document.getElementById('modal')
+    if(modal.style.display === 'none') {
+      modal.style.display = 'flex'
+      body.classList.toggle('block')
+      window.onscroll = function() {
+        window.scrollTo(0, 0)
       }
-      if (toggled === true) {
-        navbar[0].style.width = "auto";
-      } else {
-        navbar[0].style.width = "100%";
-      }
+    } else {
+      modal.style.display = 'none'
+      body.classList.toggle('block')
+      window.onscroll = function(){}
     }
   };
+  
   useEffect(() => {
     if (effectRan.current === true) {
-      toggleNavbar();
+      toggleHeader();
     }
     return () => {
       effectRan.current = true;
     };
   }, [toggled]);
-
-  useEffect(() => {
-    window.addEventListener("wheel", handleScroll);
-    return () => window.removeEventListener("wheel", handleScroll);
-  }, []);
-
-  useEffect(() => {
-    window.addEventListener("wheel", hideOnScroll);
-    return () => window.removeEventListener("wheel", hideOnScroll);
-  }, []);
-
-  useEffect(() => {
-    toggleHeader();
-  }, [visible]);
- */
   return (
     <>
       <header id="header">
-        <nav className={styles.navbar}>
-          <Link href="/" passHref className={styles.logo}>
+        <nav className={`${styles.navbar} ${styles.openNavbar}`}>
+          <Link
+            href="/"
+            passHref
+            className={`${styles.logo} ${styles.openLogo}`}
+          >
             <Image src={Logo} alt="" />
           </Link>
           <ul className={styles.list}>
@@ -111,13 +68,17 @@ export default function ShopHeader() {
               <Link href="/blog">Blog</Link>
             </li>
           </ul>
-          <div className={styles.cart_wrap}>
+          <div className={`${styles.cart_wrap} ${styles.openCartWrap}`}>
             <span className={styles.cart_span}>Your cart:</span>
             <div className={styles.cart_svg}></div>
-            <div className={styles.cart_number} id="cart-number">2</div>
+            <div className={styles.cart_number} id="cart-number">
+              2
+            </div>
             <button
               aria-label="toggle menu"
               className={styles.nav_toggle}
+              onClick={handleBurgerClick}
+              id="openButton"
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -133,13 +94,17 @@ export default function ShopHeader() {
                   d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
                 />
               </svg>
+            </button>
+            <div id="modal" className={styles.modal}>
               <svg
+                onClick={handleBurgerClick}
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
                 strokeWidth="1.5"
                 stroke="currentColor"
-                className={styles.closeIcon}
+                /* className={styles.closeIcon} */
+                id="closeButton"
               >
                 <path
                   strokeLinecap="round"
@@ -147,9 +112,31 @@ export default function ShopHeader() {
                   d="M6 18L18 6M6 6l12 12"
                 />
               </svg>
-            </button>
+              <ul className={styles.modal_list}>
+                <li>
+                  <a href="#contact">Contact</a>
+                </li>
+                <li>
+                  <Link href="/reservations">Reservations</Link>
+                </li>
+                <li>
+                  <Link href="/menu">Menu</Link>
+                </li>
+                <li>
+                  <Link href="/blog">Blog</Link>
+                </li>
+                <li className={styles.cart_li}>
+                  <div className={styles.cart_wrap}>
+                    <span className={styles.cart_span}>Your cart:</span>
+                    <div className={styles.cart_svg}></div>
+                    <div className={styles.cart_number} id="cart-number">
+                      2
+                    </div>
+                  </div>
+                </li>
+              </ul>
+            </div>
           </div>
-          
         </nav>
       </header>
     </>
