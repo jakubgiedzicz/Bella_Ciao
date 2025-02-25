bbf75e40a67a05efb40c7dd7526a8e98ccda8e75
JakubGiedzicz <jakubgiedzicz@gmail.com>
2025-02-25 14:54:52 +0100

    nextjs update

--- app/(regularHeader)/layout.tsx
+++ app/(regularHeader)/layout.tsx
@@ -2,6 +2,7 @@ import '@/styles/globals.css'
 import { Cormorant } from 'next/font/google'
 import Header from '@/components/header'
 import Footer from '@/components/footer'
+import { Viewport } from 'next'
 
 const cormorant = Cormorant({
   weight: ['400', '500', '600', '700'],
@@ -11,7 +12,12 @@ const cormorant = Cormorant({
 export const metadata = {
   title: 'Bella Ciao',
   description: 'NextJS app project',
-  viewport: "width=device-width, initial-scale=1, minimum-scale=1"
+}
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  minimumScale: 1,
+  colorScheme: 'light',
 }
 
 export default function Root({
